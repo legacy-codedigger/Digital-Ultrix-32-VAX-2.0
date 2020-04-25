56695b632d960b605717f93f84dc6fa7ec5b4e2f
zed-zahir <zahir.meddour@protonmail.com>
2020-04-25 01:27:39 +0100

    init

---
+++ usr.lib/sendmail/src/version.c
@@ -0,0 +1,9 @@
+
+#ifndef lint
+static	char	*sccsid = "@(#)version.c	1.2	(ULTRIX)	2/14/86";
+#endif	lint
+
+/* This module defines, thru sccs, the current running
+ * version of sendmail.
+ */
+char	Version[] = "1.2";
